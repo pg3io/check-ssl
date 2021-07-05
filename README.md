ccf67d3f53cc5ae5a2b34067b0ccfe589b4a35bb
Paul Riba <paul.riba@epitech.eu>
2021-07-05 17:43:10 +0200

    {+} add tags output and refresh README + list.yml {+}

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Vérifie l'expiration d'un certificat SSL pour des urls donnés
 
 ## Input
 
-Lister vos urls dans un fichier "txt"
+Lister vos urls dans un fichier "yml"
 
 ## Output
 
@@ -19,20 +19,20 @@ Lister vos urls dans un fichier "txt"
 ## Execution
 
 ```
-export LIST={PWD}/sources/list.txt
-python check_ssl.py
+export LIST=./sources/list.yml
+python3 check_ssl.py
 ```
 
 ## Docker
 ```
 docker build -t pg3io/ssl:0.1 . --no-cache
-docker run -d -e LIST=/sources/list.txt --name checkSSL pg3io/ssl:0.1
+docker run -d -e LIST=/sources/list.yml --name checkSSL pg3io/ssl:0.1
 ```
 
 ou simplement en récupérant l'image sur notre registry
 
 ```
-docker run -d -e LIST=/sources/list.txt --name checkSSL registry.pg3.io:5000/pg3io/ssl:0.1
+docker run -d -e LIST=/sources/list.yml --name checkSSL registry.pg3.io:5000/pg3io/ssl:0.1
 ```
 
 # License
@@ -42,4 +42,5 @@ docker run -d -e LIST=/sources/list.txt --name checkSSL registry.pg3.io:5000/pg3
 This project is licensed under the [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0) license - see the [LICENSE](LICENSE) file for details.
 
 # Author Information
-This role was created in 21/12/2018 by [PG3](https://pg3.io)
+This role was created in 21/12/2018 by [PG3](https://pg3.io)\
+Maintained in 05/07/2021 by [PG3](https://pg3.io)\
--- check_ssl.py
+++ check_ssl.py
@@ -5,7 +5,7 @@ import ssl
 import json
 from time import sleep
 from urllib.request import socket
-from datetime import datetime
+from datetime import datetime, date
 import yaml
 
 filesource = os.environ['LIST']
@@ -23,12 +23,15 @@ def date_converter(o):
     if isinstance(o, datetime):
         return o.__str__()
 
-def json_format(url, expiration, dns):
+def json_format(url, expiration, dns, delta, tags):
     data = {}
 
     data['url'] = url
     data['expiration'] = expiration
     data['dns'] = dns
+    data['delta'] = delta.days
+    if tags != []:
+        data['tags'] = tags
     data = json.dumps(data, default=date_converter)
 
     print(data)
@@ -44,6 +47,7 @@ def format_url(base_url):
 def main():
     while 42:
         data = open_file()
+        today = datetime.today()
 
         for url in data['website']:
             if url['ssl'] == True:
@@ -57,11 +61,18 @@ def main():
                         data = ssock.getpeercert()
 
                 expiration = datetime.utcfromtimestamp(ssl.cert_time_to_seconds(data['notAfter']))
+                delta = expiration - today
                 dns = str(data['subjectAltName'])
                 dns = dns.replace('(', '').replace(')', '').replace("'", '').replace('DNS', '').replace(' ', '')
                 dns = dns[1:]
                 dns = dns.split(',,')
-                json_format(hostname, expiration, dns)
+
+                try:
+                    tags = url['tags']
+                except:
+                    tags = []
+
+                json_format(hostname, expiration, dns, delta, tags)
 
         try:
             data['ssl_delay']
@@ -69,7 +80,6 @@ def main():
             sleep(3600)
         else:
             sleep(int(data['ssl_delay']))
-
 
 
 if __name__ == '__main__':
