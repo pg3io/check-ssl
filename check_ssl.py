87840d7f5dd61dfdefe97f04e3679e39a4e2f8d7
BuildTools <unconfigured@null.spigotmc.org>
2021-07-07 14:32:45 +0200

    ADD Try except for some Dict values, logging InfluxDB connection, logging InfluxDB connection Error

--- check_ssl.py
+++ check_ssl.py
@@ -3,12 +3,21 @@
 import os
 import ssl
 import json
-from time import sleep
-from urllib.request import socket
-from datetime import datetime, date
+import influxdb_client
 import yaml
+from time import sleep, time
+import socket
+from datetime import datetime, date
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-filesource = os.environ['LIST']
+
+try:
+    filesource = os.environ['LIST']
+except KeyError:
+    print('Missing LIST environment variable, exiting.')
+    exit(1)
+
 
 def open_file():
     with open(filesource, 'r') as stream:
@@ -16,7 +25,6 @@ def open_file():
             file = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-
     return file
 
 def date_converter(o):
@@ -33,7 +41,6 @@ def json_format(url, expiration, dns, delta, tags):
     if tags != []:
         data['tags'] = tags
     data = json.dumps(data, default=date_converter)
-
     print(data)
 
 def format_url(base_url):
@@ -44,18 +51,65 @@ def format_url(base_url):
 
     return new_url
 
+def connect_to_influxdb():
+    try:
+        influxdb_host = os.environ['INFLUXDB-HOST']
+    except:
+        print('INFLUXDB-HOST environnement variable is not set, running without InfluxDB')
+        return None
+    else:
+        if (influxdb_host != None and influxdb_host != ''):
+            try:
+                os.environ['INFLUXDB-TOKEN']
+                os.environ['INFLUXDB-ORG']
+                os.environ['INFLUXDB-BUCKET']
+            except:
+                print('INFLUXDB-HOST is defined please define INFLUX-TOKEN, INFLUX-ORG and INFLUX-BUCKET environnement variables.')
+                exit(1)
+        else:
+            print('INFLUXDB-HOST environnement variable is empty, running without InfluxDB')
+            return None
+        if (influxdb_host != None and influxdb_host != ''):
+            client = InfluxDBClient(url=influxdb_host, token=os.environ['INFLUXDB-TOKEN'])
+            return client.write_api(write_options=SYNCHRONOUS)
+
+
+def insert_to_influxdb(write_api, influxdb_connected, hostname, expiration, dns, delta, tags):
+    new_delta = f'{delta}'.split()[0]
+    sequence = [
+        f'ssl_check,host={hostname} expiration="{expiration}",dns="{dns}",delta={new_delta}'
+    ]
+    if tags != []:
+        sequence.append(f'tags,host={hostname} tags="{tags}"')
+    while True:
+        try:
+            write_api.write(os.environ['INFLUXDB-BUCKET'], os.environ['INFLUXDB-ORG'], sequence)
+        except Exception as e:
+            print('Waiting 10 seconds for InfluxDB to start...')
+            print('Script is unable to connect :\n', str(e))
+            sleep(10)
+        else:
+            if not influxdb_connected:
+                print('Connected to  InfluxDB !')
+            break
+    return True
+
 def main():
+    influxdb_connected = False
     while 42:
         data = open_file()
         today = datetime.today()
-
+        write_api = connect_to_influxdb()
         for url in data['website']:
-            if url['ssl'] == True:
+            try:
+                tmp = url['ssl']
+            except KeyError:
+                tmp = False
+            if tmp:
                 base_url = url['url']
                 port = '443'
                 hostname = format_url(base_url)
                 context = ssl.create_default_context()
-
                 with socket.create_connection((hostname, port)) as sock:
                     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
                         data = ssock.getpeercert()
@@ -66,14 +120,14 @@ def main():
                 dns = dns.replace('(', '').replace(')', '').replace("'", '').replace('DNS', '').replace(' ', '')
                 dns = dns[1:]
                 dns = dns.split(',,')
-
                 try:
                     tags = url['tags']
                 except:
                     tags = []
-
-                json_format(hostname, expiration, dns, delta, tags)
-
+                if write_api != None:
+                    influxdb_connected = insert_to_influxdb(write_api, influxdb_connected, hostname, expiration, dns, delta, tags)
+                else:
+                    json_format(hostname, expiration, dns, delta, tags)
         try:
             data['ssl_delay']
         except:
